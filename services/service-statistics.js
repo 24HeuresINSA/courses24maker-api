8a610ee6ceb7081689e7598eeac033dd1a8ff6c2
John Ratignier <john.ratignier@gmail.com>
2019-11-14 15:23:00 +0100

    Bugfix export databaser

--- services/service-statistics.js
+++ services/service-statistics.js
@@ -59,9 +59,9 @@ function getSQLJointureToExportTheWholeDatabase (){
 	return "SELECT participant_id, participant_name, participant_surname, participant_birthdate, participant_email, participant_telephone, participant_payment, participant_medical_certificate_valid, participant_student_certificate_valid, participant_tee_shirt_size, participant_comment, participant_message, " +
 		"category_label, category_price_regular, category_price_student, category_type, team_id, team_name, team_valid, " +
 		"participant.createdAt as participant_creationDate, participant.updatedAt as participant_updatedDate, team.createdAt as team_creationDate\n" +
-		"FROM `courses24maker-api-dev`.participant " +
-		"LEFT JOIN `courses24maker-api-dev`.team ON participant.participant_team_id = team.team_id " +
-		"LEFT JOIN `courses24maker-api-dev`.category ON team.team_category_id = category.category_id"
+		"FROM participant " +
+		"LEFT JOIN team ON participant.participant_team_id = team.team_id " +
+		"LEFT JOIN category ON team.team_category_id = category.category_id"
 };
 
 module.exports = {
